d91d2a4daef1fff8b22de65c39aa5617ad4a86a5
eci al <lecilimb@gmail.com>
2020-11-04 11:45:27 -0800

    user bisa drag and drop lagi + sudah ada suara feedback

--- BacarakaV1/TebakAksara.swift
+++ BacarakaV1/TebakAksara.swift
@@ -13,24 +13,31 @@ class TebakAksara: UIViewController {
 //        var player: AVAudioPlayer?
     var suaraBenar = AVAudioPlayer()
     var suaraSalah = AVAudioPlayer()
+    var jawabanBenar = AVAudioPlayer()
     
     @IBOutlet weak var KartuAksara: UIImageView!
     @IBOutlet weak var bekasBenar: UIImageView!
     @IBOutlet weak var bekasSalah: UIImageView!
+    @IBOutlet weak var bekasAtas: UIImageView!
     @IBOutlet weak var HaBenar: UIImageView!
     @IBOutlet weak var HaBenar1: UIImageView!
     @IBOutlet weak var SpeakerBenar: UIButton!
     @IBOutlet weak var SpeakerSalah: UIButton!
     
     var KartuAksaraOrigin:CGPoint!
+    var HaBenarOrigin:CGPoint!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         suara()
         
         addPanGesture(view: KartuAksara)
+        addPanGesture2(view: HaBenar1)
         KartuAksaraOrigin = KartuAksara.frame.origin
+        HaBenarOrigin = HaBenar.frame.origin
         view.bringSubviewToFront(KartuAksara)
+        view.bringSubviewToFront(HaBenar1)
     }
     
     @IBAction func benarDidTapped(_ sender: Any) {
@@ -43,10 +50,15 @@ class TebakAksara: UIViewController {
     
     
     func addPanGesture(view:UIView) {
-        
         let pan = UIPanGestureRecognizer(target: self, action: #selector (TebakAksara.handlePan(sender:)))
         view.addGestureRecognizer(pan)
     }
+    
+    func addPanGesture2(view:UIView) {
+        let secondPan = UIPanGestureRecognizer(target: self, action: #selector(TebakAksara.handleSecondPan(gesture:)))
+        view.addGestureRecognizer(secondPan)
+    }
+    
     
     // Refactor
     @objc func handlePan(sender: UIPanGestureRecognizer) {
@@ -56,14 +68,33 @@ class TebakAksara: UIViewController {
             moveViewWithPan(view: fileView, sender: sender)
         case .ended:
             if fileView.frame.intersects(bekasBenar.frame) {
-                deleteView(view: fileView)
+                deleteView(view: fileView, status: true)
                 showHa(view: HaBenar)
-                
+                jawabanBenar.play()
             } else if fileView.frame.intersects(bekasSalah.frame){
-                deleteView(view: fileView)
-                showHa1(view: HaBenar1)
+                deleteView(view: fileView, status: true)
+                showHa1(view: HaBenar1, status: true)
             } else {
                 returnViewToOrigin(view: fileView)
+            }
+            
+        default:
+            break
+        }
+    }
+    
+    @objc func handleSecondPan(gesture: UIPanGestureRecognizer) {
+        let newView = gesture.view!
+        switch gesture.state {
+        case .began, .changed:
+          
+            moveViewWithPan2(view: newView, gesture: gesture)
+        case .ended:
+            if newView.frame.intersects(bekasAtas.frame) {
+                returnViewToOrigin(view: newView)
+            } else if newView.frame.intersects(bekasBenar.frame) {
+                returnViewToRight(view: newView)
+                jawabanBenar.play()
             }
             
         default:
@@ -77,25 +108,43 @@ class TebakAksara: UIViewController {
         sender.setTranslation(CGPoint.zero, in: view)
     }
     
+    func moveViewWithPan2(view: UIView, gesture: UIPanGestureRecognizer) {
+        let newTranslation = gesture.translation(in: view)
+        view.center = CGPoint(x: view.center.x + newTranslation.x, y: view.center.y + newTranslation.y)
+        gesture.setTranslation(CGPoint.zero, in: view)
+    }
+    
     func returnViewToOrigin(view: UIView) {
         UIView.animate(withDuration: 0.3, animations: {
             view.frame.origin = self.KartuAksaraOrigin
         })
     }
     
-    func deleteView(view: UIView) {
+    func returnViewToRight(view:UIView) {
+        UIView.animate(withDuration: 0.3, animations: {
+            view.frame.origin = self.HaBenarOrigin
+        })
+    }
+    
+    func deleteView(view: UIView, status: Bool) {
         UIView.animate(withDuration: 0.3, animations: {
             view.alpha = 0.0
         })
     }
     
+//    func undeleteView(view: UIView, status: Bool) {
+//        UIView.animate(withDuration: 0.3, animations: {
+//            view.alpha = 1
+//        })
+//    }
+//
     func showHa(view: UIView) {
         UIView.animate(withDuration: 0.3, animations: {
             self.HaBenar.alpha = 1
         })
     }
     
-    func showHa1(view: UIView) {
+    func showHa1(view: UIView, status: Bool) {
         UIView.animate(withDuration: 0.3, animations: {
             self.HaBenar1.alpha = 1
         })
@@ -109,6 +158,11 @@ class TebakAksara: UIViewController {
         }
         do {
             suaraSalah = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "suaraLa", ofType: "mp3")!))
+        } catch {
+            print (error)
+        }
+        do {
+            jawabanBenar = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "dndTrue", ofType: "mp3")!))
         } catch {
             print (error)
         }
